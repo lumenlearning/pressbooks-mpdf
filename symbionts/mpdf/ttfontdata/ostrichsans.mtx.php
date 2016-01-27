3d8032b28a57e40ac8f314d3db6c160736c0e06e
monkecheese <bryan.borgeson@gmail.com>
2016-01-26 18:37:41 -0800

    add ostrich fonts, which are necessary to export pdfs

---
+++ symbionts/mpdf/ttfontdata/ostrichsans.mtx.php
@@ -0,0 +1,38 @@
+<?php
+$name='OstrichSans-Medium';
+$type='TTF';
+$desc=array (
+  'CapHeight' => 674,
+  'XHeight' => 674,
+  'FontBBox' => '[-20 -40 605 675]',
+  'Flags' => 4,
+  'Ascent' => 675,
+  'Descent' => -40,
+  'Leading' => 0,
+  'ItalicAngle' => 0,
+  'StemV' => 109,
+  'MissingWidth' => 210,
+);
+$unitsPerEm=1000;
+$up=-75;
+$ut=50;
+$strp=250;
+$strs=50;
+$ttffile='/Users/bryan/dev/pandela/wp-content/plugins/pressbooks-mpdf/symbionts/mpdf/ttfonts/ostrich-regular.ttf';
+$TTCfontID='0';
+$originalsize=12476;
+$sip=false;
+$smp=false;
+$BMPselected=false;
+$fontkey='ostrichsans';
+$panose=' 0 0 2 0 5 8 0 0 0 2 0 4';
+$haskerninfo=false;
+$haskernGPOS=false;
+$hassmallcapsGSUB=false;
+$fontmetrics='win';
+// TypoAscender/TypoDescender/TypoLineGap = 750, -250, 0
+// usWinAscent/usWinDescent = 675, -40
+// hhea Ascent/Descent/LineGap = 750, -250, 0
+$useOTL=0x0000;
+$rtlPUAstr='';
+?>
